6a8ea6f9bbc59d94a87a19543d5266b47ab1fdcc
Gia Dat <ngd10101992@gmail.com>
2022-08-11 10:27:47 +0900

    add file sql

---
+++ db/php_online.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 11, 2022 at 03:26 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_token`
+--
+
+CREATE TABLE `login_token` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `token` varchar(50) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `createAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `fullname` varchar(20) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `password` varchar(50) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `forgotToken` varchar(50) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `activeToken` varchar(50) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 0,
+  `createAt` datetime DEFAULT NULL,
+  `updateAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_token`
+--
+ALTER TABLE `login_token`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login_token`
+--
+ALTER TABLE `login_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `login_token`
+--
+ALTER TABLE `login_token`
+  ADD CONSTRAINT `login_token_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
